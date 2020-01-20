d19f975b734e1ebe3a270e308834992c44777326
lobana alarabi <loubanaeng@gmail.com>
2020-01-20 21:13:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+function writeCards(persons,eventName){
+  for (let i = persons.length-1; i >= 0; i--) {
+   
+    console.log(`Thank you, ${persons[i]}, for the wonderful surprise gift!`);
+    persons[i]=`Thank you, ${persons[i]}, for the wonderful surprise gift!`;
+    
+  }
+ 
+  return persons;
+}
+
+const persons = ['Lisa', 'Kaitlin', 'Jan'];
+const eventName="surprise";
+writeCards(persons, eventName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function countDown(num)
+{
+  console.log()
+}
